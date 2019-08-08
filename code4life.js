ee67880358752c7f3f71764cf60d6a7065f6a996
Tomas Ferreira <tomas.ferreira@fruitionpartners.nl>
2019-08-09 00:26:26 +0200

    added basic project selection logic

--- code4life.js
+++ code4life.js
@@ -1,95 +1,4 @@
-// Steps:
-// - Collect sample data files from the SAMPLES module.
-// - Analyze sample data files at the DIAGNOSIS module to get a list of molecules for the associated medicine.
-// - Gather required molecules for the medicines at the MOLECULES module.
-// - Produce the medicines at the LABORATORY module and collect your health points.
-
-// Robots:
-// Each player has one robot. Both robots have the same starting position.
-// A robot can carry up to 3 sample data files and 10 molecules.
-// A player can move their robot from one module to another by means of the GOTO module command.
-// Once the robot is at a module's interface, it can connect to it with the CONNECT command. This will have a different effect for each module.
-// Moving from one module to another takes a number of turns depending on the distance involved (see matrix below).
-// Once movement has started, it cannot be interrupted. Any commands given by the player during the turns when the robot is mobile will be ignored.
-
-// Data:
-// A sample data file is an item representing all known data on a tissue sample collected from an untreated patient. Researching this sample may ultimately lead to the production of medicine to prolong the lives of all patients with the same ailment.
-// Sample data files can be in one of two states: undiagnosed (initial state) or diagnosed.
-// A diagnosed sample data is associated with the list of molecules needed to produce the medicine for that sample.
-// Each sample data file has a rank: 1, 2 or 3. The higher the rank, the more health points you will get from the medicine - but more molecules will be needed to produce the medicine.
-
-// Molecule types: A, B, C, D, E
-
-// Modules:
-// The samples module:
-// - Connecting to this module with CONNECT rank, where rank is an integer between 1 and 3, will transfer an undiagnosed sample data file of rank rank to your robot.
-
-// The diagnosis machine:
-// - Connecting to this module with CONNECT id:
-//   - where id is the identifier of an undiagnosed sample data file the player is carrying, will change the sample's state to diagnosed.
-//   - where id is the identifier of a diagnosed sample data file the player is carrying, will transfer the sample data from the player to the cloud, where it will remain until a player takes it.
-//   - where id is the identifier of a diagnosed sample data file stored in the cloud, will transfer the sample data from the cloud to the player.
-
-// The molecule distribution module:
-// Connecting to this module with CONNECT type, where type is one of the molecule types, will transfer an available molecule to your robot of the desired type.
-// The terminal will only provide a maximum of 5 molecules per type, until molecules are spent in the lab.
-
-// The laboratory module:
-// To use this module, the player's robot must be carrying a sample data file as well as the required amount of molecules for producing that sample's medicine.
-// Connecting to this module with CONNECT id where id is the identifier of a sample data the player can research, will have several effects:
-// - The sample data id as well as the associated molecules are removed from play.
-// - The players scores as many points as the sample's health points.
-// - The player acquires molecule expertise: the robot will need 1 less molecule of the type specified by the sample for producing all subsequent medicines.
-
-// Concurrency:
-// In the event that both players try to take sample data from the cloud on the same turn, only the player who had previously diagnosed this sample will successfully complete the transfer.
-// In the event that both players request the last molecule of a given type, the module will provide an extra molecule but will wait for at least 2 molecules of that type to be spent in the lab before providing new ones.
-
-// Science projects
-// In addition to scoring points by helping Roche create new medicine for untreated diseases, a player may also further apply medical science by completing Science projects.
-// Each science project is worth 50 health points. It can be completed by either player.
-// Each game starts out with 3 random active science projects. To complete one, players must gather the required amount of molecule expertise for each type (A,B,C,D & E).
-
-// Constraints:
-// Health points scored with a rank 1 sample = 1 or 10
-// Health points scored with a rank 2 sample = 10, 20 or 30
-// Health points scored with a rank 3 sample = 30, 40 or 50
-// 3≤ Total molecule cost for a rank 1 sample ≤5
-// 5≤ Total molecule cost for a rank 2 sample ≤8
-// 7≤ Total molecule cost for a rank 3 sample ≤14
-// Response time for first turn ≤ 1000ms
-// Response time for one turn ≤ 50ms
-
-// Initialization input:
-// Line 1:projectCount, number of science projects.
-// Next projectCount lines: 5 integers, the required amount of molecule expertise needed for each type.
-
-// Input for one game turn:
-// For each player, 1 line: 1 string followed by 12 integers (you are always the first player):
-// target: module where the player is or is going to.
-// eta: number of turns before reaching the module (if greater than 0 the player's action will be ignored).
-// score: the player's number of health points
-// storageA, storageB, storageC, storageD, storageE: number of molecules held by the player for each molecule type.
-// expertiseA, expertiseB, expertiseC, expertiseD, expertiseE: the molecule expertise for each molecule type.
-
-// Next line:availableA, availableB, availableC, availableD, availableE: 5 integers, the number of available molecules for each type.
-
-// Next line:sampleCount: the number of samples currently in the game.
-// Next sampleCount lines:
-// sampleId: unique id for the sample.
-// carriedBy: 0 if the sample is carried by you, 1 by the other robot, -1 if the sample is in the cloud.
-// rank: rank of the sample 1, 2 or 3.
-// gain: expertise molecule gained when researching this sample A B C D or E.
-// health: number of health points you gain from this sample.
-// costA, costB, costC, costD, costE: number of molecules of each type needed to research the sample
-
-// Output for one game turn:
-// Each turn issue one of the following command:
-// GOTO module: move towards the target module.
-// CONNECT id/type/ rank: connect to the target module with the specified sample id or retrieve a sample of a given rank at the SAMPLES module, or molecule of the given type at the MOLECULES module.
-// WAIT: do nothing.
-
-const DEBUG = false;
+const DEBUG = true;
 if (!DEBUG) {
 	printErr = function () {
 		return;
@@ -147,9 +56,9 @@ const FUNCTIONS = {
 };
 
 const IS_PLAYING = true;
-const LEVEL_ONE_EXPERTISE = 0;
-const LEVEL_TWO_EXPERTISE = 3;
-const LEVEL_THREE_EXPERTISE = 6;
+// const LEVEL_ONE_EXPERTISE = 0;
+// const LEVEL_TWO_EXPERTISE = 3;
+// const LEVEL_THREE_EXPERTISE = 6;
 const LEVEL_ONE_RANK = 1;
 const LEVEL_TWO_RANK = 2;
 const LEVEL_THREE_RANK = 3;
@@ -159,10 +68,14 @@ const TWO_SAMPLES_TURN = 150;
 const LEVEL_TWO_TURN = 45;
 const LEVEL_THREE_TURN = 100;
 
+const PROJECT_TYPE = 9;
+
 const turns = [];
 
 const PROJECTS = getProjectData();
+setProject();
 printErr('PROJ', PROJECTS);
+
 let turnCounter = 0;
 while (IS_PLAYING) {
 	let turn = {};
@@ -202,11 +115,11 @@ while (IS_PLAYING) {
 }
 
 
-///////////////////////////////////////
-///////////////////////////////////////
-//// STATE-BASED ACTION FUNCTIONS: ////
-///////////////////////////////////////
-///////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+///////////////////// STATE-BASED ACTION FUNCTIONS: /////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
 function getAction(turn) {
 	if (turn.movingCounter !== 0) {
 		return getMovingTurn(turn);
@@ -339,11 +252,22 @@ function getLabTurn(turn) {
 	}
 }
 
-///////////////////////////
-///////////////////////////
-//// HELPER FUNCTIONS: ////
-///////////////////////////
-///////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+//////////////////////// HELPER FUNCTIONS: //////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+function setProject(){
+	let projects = PROJECTS;
+
+	for(let project of projects){
+		if(project.total === PROJECT_TYPE){
+			project.working = true;
+			return;
+		}
+	}
+}
+
 function getMaxSamplesNum(turn) {
 	if (turn.number >= ONE_SAMPLE_TURN) return 1;
 	if (turn.number >= TWO_SAMPLES_TURN) return 2;
@@ -367,6 +291,7 @@ function setSamplesData(turn) {
 		diagnosedCount++;
 		let isValid = true;
 		let isPossible = true;
+		let isProject = false;
 		let cost = sample.updatedCost;
 		// let health = sample.health;
 		let gain = sample.expertiseGain;
@@ -412,6 +337,9 @@ function setSamplesData(turn) {
 			isPossible = false;
 			isValid = false;
 		}
+
+		let project = PROJECTS.filter(project => project.working).pop();
+
 		sample.isPossible = isPossible;
 		sample.isValid = isValid;
 		if (isValid) validCount++;
@@ -609,7 +537,101 @@ function getProjectData() {
 		let c = parseInt(inputs[2]);
 		let d = parseInt(inputs[3]);
 		let e = parseInt(inputs[4]);
-		projects.push({ a, b, c, d, e });
+		let total = a + b + c + d + e;
+		let working = false;
+		projects.push({ a, b, c, d, e, total, working});
 	}
 	return projects;
 }
+
+
+// Steps:
+// - Collect sample data files from the SAMPLES module.
+// - Analyze sample data files at the DIAGNOSIS module to get a list of molecules for the associated medicine.
+// - Gather required molecules for the medicines at the MOLECULES module.
+// - Produce the medicines at the LABORATORY module and collect your health points.
+
+// Robots:
+// Each player has one robot. Both robots have the same starting position.
+// A robot can carry up to 3 sample data files and 10 molecules.
+// A player can move their robot from one module to another by means of the GOTO module command.
+// Once the robot is at a module's interface, it can connect to it with the CONNECT command. This will have a different effect for each module.
+// Moving from one module to another takes a number of turns depending on the distance involved (see matrix below).
+// Once movement has started, it cannot be interrupted. Any commands given by the player during the turns when the robot is mobile will be ignored.
+
+// Data:
+// A sample data file is an item representing all known data on a tissue sample collected from an untreated patient. Researching this sample may ultimately lead to the production of medicine to prolong the lives of all patients with the same ailment.
+// Sample data files can be in one of two states: undiagnosed (initial state) or diagnosed.
+// A diagnosed sample data is associated with the list of molecules needed to produce the medicine for that sample.
+// Each sample data file has a rank: 1, 2 or 3. The higher the rank, the more health points you will get from the medicine - but more molecules will be needed to produce the medicine.
+
+// Molecule types: A, B, C, D, E
+
+// Modules:
+// The samples module:
+// - Connecting to this module with CONNECT rank, where rank is an integer between 1 and 3, will transfer an undiagnosed sample data file of rank rank to your robot.
+
+// The diagnosis machine:
+// - Connecting to this module with CONNECT id:
+//   - where id is the identifier of an undiagnosed sample data file the player is carrying, will change the sample's state to diagnosed.
+//   - where id is the identifier of a diagnosed sample data file the player is carrying, will transfer the sample data from the player to the cloud, where it will remain until a player takes it.
+//   - where id is the identifier of a diagnosed sample data file stored in the cloud, will transfer the sample data from the cloud to the player.
+
+// The molecule distribution module:
+// Connecting to this module with CONNECT type, where type is one of the molecule types, will transfer an available molecule to your robot of the desired type.
+// The terminal will only provide a maximum of 5 molecules per type, until molecules are spent in the lab.
+
+// The laboratory module:
+// To use this module, the player's robot must be carrying a sample data file as well as the required amount of molecules for producing that sample's medicine.
+// Connecting to this module with CONNECT id where id is the identifier of a sample data the player can research, will have several effects:
+// - The sample data id as well as the associated molecules are removed from play.
+// - The players scores as many points as the sample's health points.
+// - The player acquires molecule expertise: the robot will need 1 less molecule of the type specified by the sample for producing all subsequent medicines.
+
+// Concurrency:
+// In the event that both players try to take sample data from the cloud on the same turn, only the player who had previously diagnosed this sample will successfully complete the transfer.
+// In the event that both players request the last molecule of a given type, the module will provide an extra molecule but will wait for at least 2 molecules of that type to be spent in the lab before providing new ones.
+
+// Science projects
+// In addition to scoring points by helping Roche create new medicine for untreated diseases, a player may also further apply medical science by completing Science projects.
+// Each science project is worth 50 health points. It can be completed by either player.
+// Each game starts out with 3 random active science projects. To complete one, players must gather the required amount of molecule expertise for each type (A,B,C,D & E).
+
+// Constraints:
+// Health points scored with a rank 1 sample = 1 or 10
+// Health points scored with a rank 2 sample = 10, 20 or 30
+// Health points scored with a rank 3 sample = 30, 40 or 50
+// 3≤ Total molecule cost for a rank 1 sample ≤5
+// 5≤ Total molecule cost for a rank 2 sample ≤8
+// 7≤ Total molecule cost for a rank 3 sample ≤14
+// Response time for first turn ≤ 1000ms
+// Response time for one turn ≤ 50ms
+
+// Initialization input:
+// Line 1:projectCount, number of science projects.
+// Next projectCount lines: 5 integers, the required amount of molecule expertise needed for each type.
+
+// Input for one game turn:
+// For each player, 1 line: 1 string followed by 12 integers (you are always the first player):
+// target: module where the player is or is going to.
+// eta: number of turns before reaching the module (if greater than 0 the player's action will be ignored).
+// score: the player's number of health points
+// storageA, storageB, storageC, storageD, storageE: number of molecules held by the player for each molecule type.
+// expertiseA, expertiseB, expertiseC, expertiseD, expertiseE: the molecule expertise for each molecule type.
+
+// Next line:availableA, availableB, availableC, availableD, availableE: 5 integers, the number of available molecules for each type.
+
+// Next line:sampleCount: the number of samples currently in the game.
+// Next sampleCount lines:
+// sampleId: unique id for the sample.
+// carriedBy: 0 if the sample is carried by you, 1 by the other robot, -1 if the sample is in the cloud.
+// rank: rank of the sample 1, 2 or 3.
+// gain: expertise molecule gained when researching this sample A B C D or E.
+// health: number of health points you gain from this sample.
+// costA, costB, costC, costD, costE: number of molecules of each type needed to research the sample
+
+// Output for one game turn:
+// Each turn issue one of the following command:
+// GOTO module: move towards the target module.
+// CONNECT id/type/ rank: connect to the target module with the specified sample id or retrieve a sample of a given rank at the SAMPLES module, or molecule of the given type at the MOLECULES module.
+// WAIT: do nothing.
